7a320971448885e32019d3825a87da1836fabe36
Joe Karasek <joseph.karasek@gmail.com>
2016-03-25 13:34:04 -0700

    create template form to create new meal entry

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,6 +11,23 @@ import { MealListComponent } from './meal-list.component';
     <div class="container">
       <h1>Calorie Counter</h1>
       <div class="container">
+        <div class="well">
+          <form class="">
+            <h3>Log a new meal item:</h3>
+            <label for="name">Name of food:</label>
+            <input type="text" name="name" value="">
+            <label for="calories">Calories:</label>
+            <input type="number" name="calories">
+            <label for="notes">Notes:</label>
+            <input type="text" name="notes" value="">
+            <label class="radio_label"><input type="radio" name="mealType" selected="selected">Breakfast</label>
+            <label class="radio_label"><input type="radio" name="mealType">Lunch</label>
+            <label class="radio_label"><input type="radio" name="mealType">Dinner</label>
+            <label class="radio_label"><input type="radio" name="mealType">Snack</label>
+            <button type="submit" name="newMealSubmit">Create New Item</button>
+          </form>
+        </div>
+
         <meal-list
           [meals]="meals">
         </meal-list>
