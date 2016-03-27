0f4caf14e8860b25961d57ce3515528959d92a77
Joe Karasek <joseph.karasek@gmail.com>
2016-03-27 13:49:10 -0700

    add edit button, toggles display of details

--- app/components/meal-item/meal-item.component.ts
+++ app/components/meal-item/meal-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { MealItem } from './../../models/MealItem.model';
 
 @Component({
@@ -6,13 +6,30 @@ import { MealItem } from './../../models/MealItem.model';
     inputs: ['meal', 'selectedMeal'],
     template: `
     <li class="well">
-        <p><strong>{{ meal.name }}:</strong></p>
-        <p *ngIf="meal === selectedMeal">{{ meal.calories }} calories</p>
-        <p *ngIf="meal === selectedMeal">{{ meal.notes }}</p>
+        <div *ngIf="!editable">
+          <button (click)="showEditMeal()">Edit Meal</button>
+          <p><strong>{{ meal.name }}:</strong></p>
+          <p *ngIf="meal === selectedMeal">{{ meal.calories }} calories</p>
+          <p *ngIf="meal === selectedMeal">{{ meal.notes }}</p>
+        </div>
+        <div *ngIf="editable">
+          <button (click)="showEditMeal()">Cancel</button>
+          <p><strong>EDITING MODE:</strong></p>
+          <p *ngIf="meal === selectedMeal">{{ meal.calories }} calories</p>
+          <p *ngIf="meal === selectedMeal">{{ meal.notes }}</p>
+        </div>
     </li>
     `
 })
 
 export class MealItemComponent {
+  public editable: boolean = false;
   public meal: MealItem;
+  public onEditMeal: EventEmitter<MealItem>;
+  constructor(){
+    // this.onEditClick = new EventEmitter();
+  }
+  showEditMeal(){
+    this.editable = !this.editable;
+  }
 }
